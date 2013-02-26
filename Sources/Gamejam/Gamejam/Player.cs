82631a0f3b731d1005a81ce6edf94607a71d7110
Angelo Geels <spansjh@gmail.com>
2013-02-26 15:55:00 +0100

    Player logics

--- Sources/Gamejam/Gamejam/Player.cs
+++ Sources/Gamejam/Gamejam/Player.cs
@@ -10,23 +10,40 @@ namespace Gamejam
   public class Player : Entity
   {
     public float ent_fSpeed = 5.0f;
+    public float ent_fAttackSpeed = 15.0f;
+    public bool ent_bAttacking = false;
 
     public Player()
     {
       SetTexture("Player.png");
       ent_vPosition = new Vector2(
         Gamejam.gam_fScreenWidth / 2,                 // x
-        Gamejam.gam_fScreenHeight - ent_vSize.Y / 2); // y
+        Gamejam.gam_fScreenHeight - ent_vSize.Y / 3); // y
     }
 
     public override void Update()
     {
-      if (Input.IsAnyKeyPressed(Keys.Left, Keys.A)) {
-        ent_vVelocity.X = -ent_fSpeed;
-      } else if (Input.IsAnyKeyPressed(Keys.Right, Keys.D)) {
-        ent_vVelocity.X = ent_fSpeed;
+      if (!ent_bAttacking) {
+        if (Input.IsAnyKeyPressed(Keys.Left, Keys.A)) {
+          ent_vVelocity.X = -ent_fSpeed;
+        } else if (Input.IsAnyKeyPressed(Keys.Right, Keys.D)) {
+          ent_vVelocity.X = ent_fSpeed;
+        } else {
+          ent_vVelocity.X = 0;
+        }
+        ent_vVelocity.Y = 0;
       } else {
         ent_vVelocity.X = 0;
+        ent_vVelocity.Y = -ent_fAttackSpeed;
+
+        if (ent_vPosition.Y <= 0) {
+          ent_bAttacking = false;
+          ent_vPosition.Y = Gamejam.gam_fScreenHeight - ent_vSize.Y / 3;
+        }
+      }
+
+      if (Input.IsAnyKeyPressed(Keys.Up, Keys.W, Keys.LeftControl)) {
+        ent_bAttacking = true;
       }
 
       base.Update();
