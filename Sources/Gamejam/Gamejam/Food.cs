b4d70a65e92deef224de87791c8064570c9a340a
Harry van Wier <harryvanwier@gmail.com>
2013-02-26 14:20:40 +0100

    Added base for Food object and test texture for it

---
+++ Sources/Gamejam/Gamejam/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Gamejam
+{
+  class Food : Entity
+  {
+    static float Gravity = 1f;
+    public override void Update()
+    {
+
+      base.Update();
+    }
+    public override void Render()
+    {
+      Gamejam.SpriteBatch.Draw(Content.GetTexture("TestFood.png"), ent_vPosition, Color.White);
+
+      base.Render();
+    }
+  }
+}
