d9a75d0e02b40f015abf401e0211dfbcc4ee3f65
Angelo Geels <spansjh@gmail.com>
2013-02-26 15:02:53 +0100

    Added Input class

---
+++ Sources/Gamejam/Gamejam/Input.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Gamejam
+{
+  public class Input
+  {
+    public static bool IsKeyPressed(Keys key)
+    {
+      return Keyboard.GetState().IsKeyDown(key);
+    }
+
+    public static bool IsKeyReleased(Keys key)
+    {
+      return !IsKeyPressed(key);
+    }
+
+    public static bool IsMouseLeftPressed()
+    {
+      return Mouse.GetState().LeftButton == ButtonState.Pressed;
+    }
+
+    public static bool IsMouseRightPressed()
+    {
+      return Mouse.GetState().RightButton == ButtonState.Pressed;
+    }
+  }
+}
