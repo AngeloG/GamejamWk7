147d042fb1d41ad6423d3a8ba8e21a3b592cd107
Harry van Wier <harryvanwier@gmail.com>
2013-02-26 16:19:46 +0100

    Food moves now

--- Sources/Gamejam/Gamejam/Food.cs
+++ Sources/Gamejam/Gamejam/Food.cs
@@ -8,16 +8,50 @@ namespace Gamejam
 {
   class Food : Entity
   {
-    static float Gravity = 1f;
+    public Food()
+    {
+      SetTexture("TestFood.png");
+    }
+
+    public enum Direction
+    {
+      LEFT,
+      RIGHT
+    }
+
+    public Direction curDirection = Direction.RIGHT;
     public override void Update()
     {
+      if (ent_vPosition.X > Gamejam.gam_fScreenWidth)
+      {
+        ToggleDirection();
+      }
+      else if (ent_vPosition.X < 0)
+      {
+        ToggleDirection();
+      }
+
+      ent_vPosition.X += curDirection == Direction.RIGHT ? 1 : -1; 
 
       base.Update();
     }
+
+    private void ToggleDirection()
+    {
+      if (curDirection == Direction.LEFT)
+      {
+        curDirection = Direction.RIGHT;
+      }
+      else
+      {
+        curDirection = Direction.LEFT;
+      }
+    }
+
     public override void Render()
     {
-      Gamejam.SpriteBatch.Draw(CContent.GetTexture("TestFood.png"),
-        ent_vPosition, Color.White);
+      Gamejam.SpriteBatch.Draw(ent_texTexture,
+        GetCollision(), Color.White);
 
       base.Render();
     }
